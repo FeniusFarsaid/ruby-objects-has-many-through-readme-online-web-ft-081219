654da417c8520dae75161786fbda039f6724e76e
David Ingram <david@ingramaviation.com>
2019-09-04 20:40:29 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -17,8 +17,10 @@ class Customer
     Meal.new(waiter, self, total, tip)
   end
   
-  def meals(customer)
-    Customer(customer).meals.all 
-  end 
+  def meals
+  Meal.all.select do |meal|
+    meal.customer == self
+  end
+end
 
 end
